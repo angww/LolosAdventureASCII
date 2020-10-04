db1441abe02b16c2a4ac8278f393f6f9b63ce558
Andrei Bereta <andreibereta@gmail.com>
2020-10-04 17:06:41 -0300

    Alteração nos menus e definição do tamanho do grid interno

--- includes/defines_lolo.h
+++ includes/defines_lolo.h
@@ -1,16 +1,18 @@
+#pragma once
 
 /* JANELAS */ 
 #define JANELA_MAX_X 80
 #define JANELA_MAX_Y 25
 #define JANELA_MSEC_ANIMACAO 10
 
-
+/* JANELA DO MAPA */
+#define JANELA_GRID_MAX_X 53
+#define JANELA_GRID_MAX_Y 21
 
 /* STRINGS */ 
 #define STR_TITULO              "LoLo's Adventure - ASCII"
 #define STR_MENU_PRINCIPAL      "Menu Principal"
 #define STR_MENU_VOLTAR         "Voltar para o menu"
-
 
 /* GAME */
 #define LOLO        '@'
@@ -20,3 +22,4 @@
 #define CORACAO     'C'
 #define BAU         'B'
 #define AGUA        'A'
+#define LIVRE       ' '
