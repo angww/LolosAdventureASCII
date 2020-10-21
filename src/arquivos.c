229c35c8417a8f1770eaf6a88eace73011c3ce6b
Andrei Bereta <andreibereta@gmail.com>
2020-10-21 01:14:41 -0300

    Simplificação nas funções de arquivos

--- src/arquivos.c
+++ src/arquivos.c
@@ -1,18 +1,92 @@
 #include "arquivos.h"
 
-int existe_pasta(char *filename)
+int verifica_arquivos(void)
 {
-    /* Tenta abrir a pasta */
-    DIR *dir;
-    dir = opendir(filename);
+    int ret;
 
+    /* Pasta necessária */
+    ret = arquivo_existe(NIVEIS_PASTA);
+    if ( ret ) {
+        return 1;
+    }
+
+    /* Se não existe pode ser criada */
+    ret = arquivo_existe(PASTA);
+    if ( ret ) {
+        ret = cria_pasta(PASTA);
+        if ( ret ) {
+            return 2;
+        }
+    }
+
+    return ret;
+}
+
+int arquivo_existe(char *filename)
+{
     int errsv = SUCCESS;
+    int ret;
 
-    /* errsv = errno apenas se houve erro, errno pode conter valor antigo */
-    if ( dir == NULL ) {
+    ret = access(filename, F_OK);
+
+    if ( ret != SUCCESS ) {
         errsv = errno;
+
+        #ifdef DEBUG
+            write_debug_messagef("Erro: %s: %s", strerror(errno), filename);
+        #endif
     } else {
-        closedir(dir);
+        #ifdef DEBUG
+            write_debug_messagef("Arquivo %s existe", filename);
+        #endif
+    }
+
+    return errsv;
+}
+
+int arquivo_acessivel(char *filename)
+{
+    int errsv = SUCCESS;
+    int ret;
+
+    ret = access(filename, R_OK | W_OK);
+
+    if ( ret != SUCCESS ) {
+        errsv = errno;
+
+        #ifdef DEBUG
+            write_debug_messagef("Erro: %s: %s", strerror(errno), filename);
+        #endif
+    } else {
+        #ifdef DEBUG
+            write_debug_messagef("Arquivo %s acessivel", filename);
+        #endif
+    }
+
+    return errsv;
+}
+
+int cria_arquivo(char *filename)
+{
+    int errsv = SUCCESS;
+    int fd;
+
+    mode_t mode = PERMISSOES;
+
+    #ifdef DEBUG
+        write_debug_messagef("Criando arquivo %s", filename);
+    #endif
+
+    fd = open(filename, O_WRONLY | O_CREAT , mode);
+
+    if ( fd == -1 ){
+        errsv = errno;
+
+        #ifdef DEBUG
+            write_debug_messagef("Erro: %s: %s", strerror(errno), filename);
+        #endif
+    } else {
+        close(fd);
     }
 
     return errsv;
@@ -23,70 +97,21 @@ int cria_pasta(char *filename)
     int errsv = SUCCESS;
     int ret;
 
+    #ifdef DEBUG
+        write_debug_messagef("Criando pasta %s", filename);
+    #endif
+
     /* Tenta criar a pasta */
     ret = mkdir(filename, PERMISSOES);
 
     /* Se houve erro, armazena o errno */
     if ( ret != 0 ) {
         errsv = errno;
+
+        #ifdef DEBUG
+            write_debug_messagef("Erro: %s: %s", strerror(errno), filename);
+        #endif
     }
 
     return errsv;
-}
-
-int tenta_abrir_ou_criar_pasta(char *filename)
-{
-    #ifdef DEBUG
-        write_debug_messagef("Tentanto abrir pasta %s", filename);
-    #endif
-
-    /* Verifica se a pasta existe */
-    int ret = existe_pasta(filename);
-
-    /* Verifica o retorno, se a pasta não existe, tenta criá-la */
-    switch ( ret ) {
-        case SUCCESS:
-            #ifdef DEBUG
-                write_debug_messagef("A pasta %s existe.", filename);
-            #endif
-
-            break;
-        case ENOENT:
-            #ifdef DEBUG
-                write_debug_messagef("Tentando criar a pasta %s", filename);
-            #endif
-
-            /* Tenta criar a pasta */
-            ret = cria_pasta(filename);
-
-            /* Verifica o retorno */
-            switch ( ret ) {
-                case SUCCESS:
-                    #ifdef DEBUG
-                        write_debug_messagef("A pasta %s foi criada.", filename);
-                    #endif
-
-                    break;
-                default:
-                    #ifdef DEBUG
-                        write_debug_messagef("Erro: %s: %s", strerror(ret),
-                            filename);
-                    #endif
-
-                    fprintf(stderr, "Erro: %s: %s\n", strerror(ret), filename);
-                    break;
-                break;
-            }
-
-            break;
-        default:
-            #ifdef DEBUG
-                write_debug_messagef("Erro: %s: %s", strerror(ret), filename);
-            #endif
-
-            fprintf(stderr, "Erro: %s: %s\n", strerror(ret), filename);
-            break;
-    }
-
-    return ret;
 }
--- src/arquivos.h
+++ src/arquivos.h
@@ -7,6 +7,7 @@
 #include <dirent.h>    /* opendir */
 #include <sys/types.h> /* opendir precisa do tipo DIR */
 #include <sys/stat.h>  /* mkdir */
+#include <fcntl.h>     /* open e O_RDONLY */
 
 #ifdef DEBUG
 #include "debug.h"
@@ -21,61 +22,18 @@
 #define SUCCESS 0
 
 /*
-* Permite a leitura, escrita e execução para usuário e grupo, para outros
-* permite a leitura e a execução
-*/
-#define PERMISSOES S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH
-
-/*
- * Verifica a existência de uma pasta, retorna 0 caso exista, se não retorna um
- * código de erro
+ * Permite a leitura e escrita para usuário, para grupo e outros permite a
+ * leitura
  */
-int existe_pasta(char *filename);
+#define PERMISSOES S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+
+/* Verifica se determinado arquivo ou pasta existe */
+int arquivo_existe(char *filename);
+/* Verifica se podemos ler e escrever o arquivo */
+int arquivo_acessivel(char *filename);
+/* Cria um arqivo arquivo */
+int cria_arquivo(char *filename);
 /* Cria uma pasta, retorna 0 caso consiga, se não retorna um código de erro */
 int cria_pasta(char *filename);
-/*
- * Função que verifica a existência de uma pasta, caso não exista tenta criá-la,
- * caso consiga retorna 0, se não retorna um código de erro e imprime em stderr
- * uma mensagem
- */
-int tenta_abrir_ou_criar_pasta(char *filename);
-
-/* Abaixo há uma lista de erros que são esperados e notificados */
-
-/*
- * Para a função cria_pasta espera-se:
- *
- * EROFS -> read-only file system
- * ENOSPC -> sem espaço
- * EEXIST -> ja existe
- * EACCES -> nao pode escrever
- */
-
-/*
- * Para a função existe_pasta espera-se:
- *
- * EACCES -> sem permissao de acesso
- * ENOENT -> Nao existe
- * ENOTDIR -> Nao é uma pasta
- */
-
-/*
- * Outros valores que podem ser atribuídos a errno(para ver todos veja o manual
- * 3 errno):
- *
- * ENOTDIR       -> Not a directory
- * EACCES        -> Permission Denied
- * EEXIST        -> File Exists
- * EISDIR        -> Is a directory
- * ENOENT        -> No such file or directory
- * ENOTEMPTY     -> Directory not empty
- * EPERM         -> Operation not permitted
- * EROFS         -> Read-only filesystem
- * ETXTBUSY      -> Text file busy
- * ENAMETOOLONG  -> Filename too long
- * EIO           -> Input/output error
- * EFBIG         -> File too big
- * EBUSY         -> Device on resource busy
- * EADDRINUSE    -> Address already in use
- * EADDRNOTAVAIL -> Address not avaible
-*/
+/* Função que checa se os arquivos necessários para rodar existem */
+int verifica_arquivos(void);
