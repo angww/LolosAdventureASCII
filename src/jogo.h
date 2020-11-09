895e3f1c380e5d04b3d3721f279638b3de7197b4
Angelo Martins <angelo18@gmail.com>
2020-11-09 02:43:35 -0300

    Colocado o menu PAUSE apertando a tecla ESC.
    Teve que ser atualizado algumas partes para atualizar o mapa->elementos.

--- src/jogo.h
+++ src/jogo.h
@@ -10,6 +10,14 @@
 #include <ncurses.h>
 #include "selecionar_opcoes_posicao.h"
 
+/* Definições do sub-menu de pausa */
+#define OPCAO_CONTINUAR_JOGANDO 0
+#define OPCAO_SALVAR            1
+#define OPCAO_SAIR              2
+
+/* Tecla 254 escolhida arbitrariamente para GAME_OVER */
+#define GAME_OVER      254
+
 /* Executa os mapa apartir do definido em gravacao */
 int joga_mapas(gravacao_st *gravacao);
 /*
@@ -22,7 +30,7 @@ int loop_jogo(mapa_st *mapa, gravacao_st *gravacao);
 /* Primeira exibição completa do mapa */
 void exibe_grid(char grid[JOGO_JANELA_Y][JOGO_JANELA_X]);
 /* Movimenta o inimigo na tela, retorna 0 se não se moveu */
-int movimenta_inimigo(inimigo_st *inimigo);
+int movimenta_inimigo(inimigo_st *inimigo, mapa_st *mapa);
 /* Abre um novo jogo */
 int novojogo(void);
 /* Carrega um save e continuar a jogar nele */
@@ -41,3 +49,13 @@ ponto_st muda_pos(ponto_st pos, int key);
 int movimenta_lolo(lolo_st *lolo, int key);
 /* Retorna mov em um posição adjacente válida */
 int prox_mov(char adjacentes[4], int mov);
+
+/* Exibe o menu enquanto o jogo está parado. */
+int exibe_menu_pause(void);
+
+/* Processa os retornos (Voltar, Salvar e Sair) do menu de pause do jogo. */
+int processa_menu_pause(void);
+
+/* Atuliza o mapa no determinado ponto 'pos' colocando o item_grid */
+int atualiza_grid_mapa(mapa_st *mapa, ponto_st pos, int item_grid);
+
